d6329b2555de0613220183297ef69b70fbe97ec4
Wei Fang <Wei.Fang@innovapost.com>
2020-11-22 08:50:25 -0500

    updated to kafka 2.6

--- src/main/java/com/shapira/examples/streams/clickstreamenrich/ClickstreamEnrichment.java
+++ src/main/java/com/shapira/examples/streams/clickstreamenrich/ClickstreamEnrichment.java
@@ -11,11 +11,10 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.kstream.JoinWindows;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.KStreamBuilder;
-import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.*;
+import org.apache.kafka.streams.StreamsBuilder;
 
+import java.time.Duration;
 import java.util.Properties;
 
 public class ClickstreamEnrichment {
@@ -26,6 +25,8 @@ public class ClickstreamEnrichment {
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "clicks");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, Constants.BROKER);
         // Since each step in the stream will involve different objects, we can't use default Serde
+        // props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.Integer().getClass().getName());
+        // props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
 
         // setting offset reset to earliest so that we can re-run the demo code with the same pre-loaded data
         // Note: To re-run the demo, you need to use the offset reset tool:
@@ -37,46 +38,47 @@ public class ClickstreamEnrichment {
         // don't use in large production apps - this increases network load
         // props.put(CommonClientConfigs.METADATA_MAX_AGE_CONFIG, 500);
 
-        KStreamBuilder builder = new KStreamBuilder();
+        StreamsBuilder builder = new StreamsBuilder();
 
-        KStream<Integer, PageView> views = builder.stream(Serdes.Integer(), new PageViewSerde(), Constants.PAGE_VIEW_TOPIC);
-        KTable<Integer, UserProfile> profiles = builder.table(Serdes.Integer(), new ProfileSerde(), Constants.USER_PROFILE_TOPIC, "profile-store");
-        KStream<Integer, Search> searches = builder.stream(Serdes.Integer(), new SearchSerde(), Constants.SEARCH_TOPIC);
+        KStream<Integer, PageView> views = builder.stream(Constants.PAGE_VIEW_TOPIC, Consumed.with(Serdes.Integer(), new PageViewSerde()));
+        KTable<Integer, UserProfile> profiles = builder.table(Constants.USER_PROFILE_TOPIC, Consumed.with(Serdes.Integer(), new ProfileSerde()));
+        KStream<Integer, Search> searches = builder.stream(Constants.SEARCH_TOPIC, Consumed.with(Serdes.Integer(), new SearchSerde()));
+
+        //debug
+        /*
+        views.print(Printed.toSysOut());
+        profiles.toStream().print(Printed.toSysOut());
+        searches.print(Printed.toSysOut());
+         */
 
         KStream<Integer, UserActivity> viewsWithProfile = views.leftJoin(profiles,
-                    (page, profile) -> {
-                        if (profile != null)
-                            return new UserActivity(profile.getUserID(), profile.getUserName(), profile.getZipcode(), profile.getInterests(), "", page.getPage());
-                        else
-                           return new UserActivity(-1, "", "", null, "", page.getPage());
-
-        });
+                (page, profile) -> {
+                    if (profile != null)
+                        return new UserActivity(profile.getUserID(), profile.getUserName(), profile.getZipcode(), profile.getInterests(), "", page.getPage());
+                    else
+                        return new UserActivity(-1, "", "", null, "", page.getPage());
+                });
 
         KStream<Integer, UserActivity> userActivityKStream = viewsWithProfile.leftJoin(searches,
-                (userActivity, search) -> {
+                (UserActivity userActivity, Search search) -> {
                     if (search != null)
                         userActivity.updateSearch(search.getSearchTerms());
                     else
                         userActivity.updateSearch("");
                     return userActivity;
                 },
-                JoinWindows.of(1000), Serdes.Integer(), new UserActivitySerde(), new SearchSerde());
+                JoinWindows.of(Duration.ofSeconds(1)), Joined.with(Serdes.Integer(), new UserActivitySerde(), new SearchSerde()));
 
-        userActivityKStream.to(Serdes.Integer(), new UserActivitySerde(), Constants.USER_ACTIVITY_TOPIC);
+        userActivityKStream.to(Constants.USER_ACTIVITY_TOPIC, Produced.with(Serdes.Integer(), new UserActivitySerde()));
 
-        KafkaStreams streams = new KafkaStreams(builder, props);
-
+        KafkaStreams streams = new KafkaStreams(builder.build(), props);
         streams.cleanUp();
-
         streams.start();
 
         // usually the stream application would be running forever,
         // in this example we just let it run for some time and stop since the input data is finite.
         Thread.sleep(60000L);
-
         streams.close();
-
-
     }
 
     static public final class PageViewSerde extends WrapperSerde<PageView> {
